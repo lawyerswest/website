31d116d1014ff44e39145f418b68746f5efd615d
bickmark <25678713+bickmark@users.noreply.github.com>
2022-04-25 22:17:13 -0700

    update colors; update logo size;

--- app/components/estates.tsx
+++ app/components/estates.tsx
@@ -84,7 +84,7 @@ export default function Example() {
             {features.map((feature) => (
               <div key={feature.name}>
                 <dt>
-                  <div className='flex items-center justify-center h-12 w-12 rounded-md bg-cyan-500 text-white'>
+                  <div className='flex items-center justify-center h-12 w-12 rounded-md bg-gray-800 text-cyan-300'>
                     <feature.icon className='h-6 w-6' aria-hidden='true' />
                   </div>
                   <p className='mt-5 text-lg leading-6 font-medium text-gray-900'>
--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -37,7 +37,7 @@ export default function Hero() {
                   <div className='flex items-center justify-between w-full md:w-auto'>
                     <Link to='/'>
                       <span className='sr-only'>Lawyers West LLP</span>
-                      <img className='h-8 w-auto sm:h-10' src={logo} />
+                      <img className='h-8 w-auto sm:h-16' src={logo} />
                     </Link>
                     <div className='-mr-2 flex items-center md:hidden'>
                       <Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500'>
